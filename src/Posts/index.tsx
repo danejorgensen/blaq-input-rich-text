32a178bec878fb92e6066ca030e24dac8f7a749d
Artem Solovev <artemsolovev1201@gmail.com>
2021-01-23 15:15:08 -0500

    fixed styling

--- src/Posts/index.tsx
+++ src/Posts/index.tsx
@@ -9,8 +9,8 @@ import {
 } from "react-admin";
 import RichTextInput from "ra-input-rich-text";
 
-import "quill/dist/quill.snow.css";
-
+import "quill-handlebars/dist/quill.handlebars.css";
+import { makeStyles } from '@material-ui/core/styles'
 import "quill-handlebars";
 
 export const PostList = (props) => (
@@ -35,46 +35,50 @@ const hashValues = [
   { id: 4, value: "LastName" },
 ];
 
-export const PostCreate = (props) => (
-  <Fragment>
-    <Create {...props}>
-      <SimpleForm>
-        <TextInput source="title" />
-        <RichTextInput
-          source="body"
-          options={{
-            modules:{
-            handlebars: {
-              allowedChars: /^[A-Za-z\sÅÄÖåäö]*$/,
-              handlebarsDenotationChars: ["{{"],
-              source: function (searchTerm, renderList, handlebarsChar) {
-                let values;
-
-                if (handlebarsChar === "{{") {
-                  values = atValues;
-                } else {
-                  values = hashValues;
-                }
-
-                if (searchTerm.length === 0) {
-                  renderList(values, searchTerm);
-                } else {
-                  const matches = [];
-                  for (let i = 0; i < values.length; i++)
-                    if (
-                      ~values[i].value
-                        .toLowerCase()
-                        .indexOf(searchTerm.toLowerCase())
-                    )
-                      matches.push(values[i]);
-                  renderList(matches, searchTerm);
-                }
+export const PostCreate = (props) => {
+  const useStyles = makeStyles({
+      createPost:{ '& div': {overflow: 'visible !important' }}
+  });
+  const classes = useStyles();
+  return (
+    <Fragment>
+      <Create className={classes.createPost} {...props}>
+        <SimpleForm >
+          <TextInput source="title" />
+          <RichTextInput
+            source="body"
+            options={{
+              modules:{
+              handlebars: {
+                allowedChars: /^[A-Za-z\sÅÄÖåäö]*$/,
+                handlebarsDenotationChars: ["{{"],
+                source: function (searchTerm, renderList, handlebarsChar) {
+                  let values;
+                  if (handlebarsChar === "{{") {
+                    values = atValues;
+                  } else {
+                    values = hashValues;
+                  }
+                  if (searchTerm.length === 0) {
+                    renderList(values, searchTerm);
+                  } else {
+                    const matches = [];
+                    for (let i = 0; i < values.length; i++)
+                      if (
+                        ~values[i].value
+                          .toLowerCase()
+                          .indexOf(searchTerm.toLowerCase())
+                      )
+                        matches.push(values[i]);
+                    renderList(matches, searchTerm);
+                  }
+                },
               },
-            },
-          }
-          }}
-        />
-      </SimpleForm>
-    </Create>
-  </Fragment>
-);
+            }
+            }}
+          />
+        </SimpleForm>
+      </Create>
+    </Fragment>
+  );
+}
